08a838fbe1806dabe6256a92457e165e0bcf53e6
vladeiko <bloodlad.hacker@yandex.ru>
2020-12-08 10:44:47 +0300

    Add Types component

---
+++ components/Types/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MainWrapper } from "./style";
+
+const Types = () => {
+  return <MainWrapper></MainWrapper>;
+};
+
+export default Types;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/index";
 import Places from "../components/Places/index";
+import Types from "../components/Types/index";
 
 export default function App() {
   return (
     <div>
       <Header />
       <Places />
+      <Types />
     </div>
   );
 }
